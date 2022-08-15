6f5c1e0705a8323e779a5f3e1bd9673747b20e99
elch0101 <34032365+elch0101@users.noreply.github.com>
2022-08-15 17:04:22 -0500

    Git

---
+++ JS/index.js
@@ -0,0 +1,69 @@
+const plants= gsap.timeline({defaults: {duration: 3}})
+
+const progressBar=gsap.timeline({scrollTrigger: {trigger: '#languages', toggleActions: "restart restart none none"}, defaults: {duration: 1.5}})
+
+const timeLine=gsap.timeline({scrollTrigger: {trigger: '.timeLine-container'}, defaults: {duration: 1}})
+
+const process=gsap.timeline({scrollTrigger: {trigger: '#process', toggleActions: "restart restart none none"}, default: {duration: 1}, transformOrigin: "center center"})
+
+const clock= gsap.timeline({defaults: {duration: .5, transformOrigin: "0% 50%", ease: "none"}})
+
+// ----- * PLANTS * -----//
+
+plants.fromTo('#background', {scale: 0, transformOrigin: "center center"}, {scale: 1, ease: "power.out", duration: 1.5})
+plants.fromTo('#plant_left', {x: -300}, {x: 0, ease: "elastic.out(1, 1)"}, '<')
+plants.fromTo('#plant_right', {y: 300}, {y: 0, ease: "elastic.out(1, 1)"}, '<')
+
+// ----- * PROGRESS-BARS * -----//
+
+progressBar.fromTo('.progress-bar', {x: -186}, {x: 0, ease: "power1.in"})
+
+// ----- * TIMELINE * -----//
+
+timeLine.fromTo('#ex1', {opacity: 0}, {opacity: 1})
+timeLine.fromTo('#ex2', {opacity: 0}, {opacity: 1})
+timeLine.fromTo('#ex3', {opacity: 0}, {opacity: 1})
+timeLine.fromTo('#ex4', {opacity: 0}, {opacity: 1})
+
+// ----- * PROCESS * -----//
+process.to('#arrow1', {x: 10, ease: "power1.in"})
+process.to('#arrow1', {x: 0, ease: "power1.in"})
+process.to('#arrow2', {x: 10, ease: "power1.in"}, '<')
+process.to('#arrow2', {x: 0, ease: "power1.in"})
+process.to('#arrow3', {x: 10, ease: "power1.in"}, '<')
+process.to('#arrow3', {x: 0, ease: "power1.in"})
+process.to('#arrow4', {x: 10, ease: "power1.in"}, '<')
+process.to('#arrow4', {x: 0, ease: "power1.in"})
+process.to('#arrow5', {x: 10, ease: "power1.in"}, '<')
+process.to('#arrow5', {x: 0, ease: "power1.in"})
+process.repeat(-1);
+
+// ------- * CLOCK * -----//
+
+function showTime(){
+    let userTime = new Date();
+    let hours = userTime.getHours();
+    let minutes = userTime.getMinutes();    
+    let seconds = userTime.getSeconds()+1; 
+    
+    if(hours>12){
+        hours-=12;
+    }
+
+    let hoursTxt = ((((360/12)*hours)+(30/60)*minutes)-90).toString().concat("_cw");
+    let minutesTxt = (((360/60)*minutes)-90).toString().concat("_cw");
+    let secondsTxt = (((360/60)*seconds)-90).toString().concat("_cw");
+
+    clock.to('#hours', {rotation: hoursTxt})
+    clock.to('#minutes', {rotation: minutesTxt}, '<')
+    clock.to('#seconds', {rotation: secondsTxt}, '<')
+    
+    setTimeout(showTime, 1000);
+}
+
+showTime();
+
+
+
+
+
---
+++ JS/logos.js
@@ -0,0 +1,2 @@
+TweenMax.from("#logo", 1.5, {scale:0, opacity: 0, transformOrigin:"center center"});
+TweenMax.to("#logo", 1.5, {scale:1, opacity: 1, transformOrigin:"center center", ease: "power2.out"});
