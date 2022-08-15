e6283b1f432932a82708552f67eb2e2af3da1345
elch0101 <34032365+elch0101@users.noreply.github.com>
2022-08-15 17:20:59 -0500

    La fruteria & sender text

--- JS/index.js
+++ JS/index.js
@@ -1,58 +1,64 @@
 const plants= gsap.timeline({defaults: {duration: 3}})
-const progressBar=gsap.timeline({scrollTrigger:{trigger: '#languages', toggleActions: "restart restart none none"},defaults: {duration: 1.5}})
-const timeLine=gsap.timeline({scrollTrigger:{trigger: '.timeLine-container'},defaults: {duration:1}})
-const arrows=gsap.timeline({scrollTrigger:{trigger: '#process', toggleActions: "restart restart none none"}, default: {duration:.5},transformOrigin:"center center"})
-const clock= gsap.timeline({defaults: {duration: .5,transformOrigin:"0%  100%"}})
 
-//Plants
-plants.fromTo('#plant_left',{x: -300}, {x:0, ease: "elastic.out(1, 1)"}, '<')
-plants.fromTo('#plant_right', {y:300}, {y:0, ease: "elastic.out(1, 1)"}, '<')
+const progressBar=gsap.timeline({scrollTrigger: {trigger: '#languages', toggleActions: "restart restart none none"}, defaults: {duration: 1.5}})
 
-//Progress-bars
-progressBar.from('.progress-bar',{x:-186, ease: "power1.in"})
-progressBar.to('.progress-bar',{x:0})
+const timeLine=gsap.timeline({scrollTrigger: {trigger: '.timeLine-container'}, defaults: {duration: 1}})
 
-//Timeline
-timeLine.fromTo('#ex1', {opacity:0}, {opacity:1})
-timeLine.fromTo('#ex2', {opacity:0}, {opacity:1})
-timeLine.fromTo('#ex3', {opacity:0}, {opacity:1})
-timeLine.fromTo('#ex4', {opacity:0}, {opacity:1})
+const process=gsap.timeline({scrollTrigger: {trigger: '#process', toggleActions: "restart restart none none"}, default: {duration: 1}, transformOrigin: "center center"})
 
+const clock= gsap.timeline({defaults: {duration: .5, transformOrigin: "0% 50%", ease: "none"}})
 
-//Process
-arrows.to('#arrow1', {x:10, ease: "power1.in"})
-arrows.to('#arrow1', {x:0, ease: "power1.in"})
-arrows.to('#arrow2', {x:10, ease: "power1.in"})
-arrows.to('#arrow2', {x:0, ease: "power1.in"})
-arrows.to('#arrow3', {x:10, ease: "power1.in"})
-arrows.to('#arrow3', {x:0, ease: "power1.in"})
-arrows.to('#arrow4', {x:10, ease: "power1.in"})
-arrows.to('#arrow4', {x:0, ease: "power1.in"})
-arrows.to('#arrow5', {x:10, ease: "power1.in"})
-arrows.to('#arrow5', {x:0, ease: "power1.in"})
-arrows.repeat(-1);
+// ----- * PLANTS * -----//
 
+plants.fromTo('#background', {scale: 0, transformOrigin: "center center"}, {scale: 1, ease: "power.out", duration: 1.5})
+plants.fromTo('#plant_left', {x: -300}, {x: 0, ease: "elastic.out(1, 1)"}, '<')
+plants.fromTo('#plant_right', {y: 300}, {y: 0, ease: "elastic.out(1, 1)"}, '<')
 
-//Clock
+// ----- * PROGRESS-BARS * -----//
+
+progressBar.fromTo('.progress-bar', {x: -186}, {x: 0, ease: "power1.in"})
+
+// ----- * TIMELINE * -----//
+
+timeLine.fromTo('#ex1', {opacity: 0}, {opacity: 1})
+timeLine.fromTo('#ex2', {opacity: 0}, {opacity: 1})
+timeLine.fromTo('#ex3', {opacity: 0}, {opacity: 1})
+timeLine.fromTo('#ex4', {opacity: 0}, {opacity: 1})
+
+// ----- * PROCESS * -----//
+process.to('#arrow1', {x: 10, ease: "power1.in"})
+process.to('#arrow1', {x: 0, ease: "power1.in"})
+process.to('#arrow2', {x: 10, ease: "power1.in"}, '<')
+process.to('#arrow2', {x: 0, ease: "power1.in"})
+process.to('#arrow3', {x: 10, ease: "power1.in"}, '<')
+process.to('#arrow3', {x: 0, ease: "power1.in"})
+process.to('#arrow4', {x: 10, ease: "power1.in"}, '<')
+process.to('#arrow4', {x: 0, ease: "power1.in"})
+process.to('#arrow5', {x: 10, ease: "power1.in"}, '<')
+process.to('#arrow5', {x: 0, ease: "power1.in"})
+process.repeat(-1);
+
+// ------- * CLOCK * -----//
+
 function showTime(){
-    var userTime = new Date();
-    var hours=userTime.getHours();
-    var minutes=userTime.getMinutes();    
-    var seconds=userTime.getSeconds()+1; 
+    let userTime = new Date();
+    let hours = userTime.getHours();
+    let minutes = userTime.getMinutes();    
+    let seconds = userTime.getSeconds()+1; 
     
     if(hours>12){
         hours-=12;
     }
 
-    var hoursTxt = ((((360/12)*hours)+(30/60)*minutes)-90).toString().concat("_cw");
-    var minutesTxt = (((360/60)*minutes)-90).toString().concat("_cw");
-    var secondsTxt = (((360/60)*seconds)-90).toString().concat("_cw");
+    let hoursTxt = ((((360/12)*hours)+(30/60)*minutes)-90).toString().concat("_cw");
+    let minutesTxt = (((360/60)*minutes)-90).toString().concat("_cw");
+    let secondsTxt = (((360/60)*seconds)-90).toString().concat("_cw");
 
-    clock.to('#hours', {rotation:hoursTxt})
-    clock.to('#minutes', {rotation:minutesTxt},'<')
-    clock.to('#seconds', {rotation:secondsTxt}, '<')
+    clock.to('#hours', {rotation: hoursTxt})
+    clock.to('#minutes', {rotation: minutesTxt}, '<')
+    clock.to('#seconds', {rotation: secondsTxt}, '<')
     
-    setTimeout(showTime,1000);
+    setTimeout(showTime, 1000);
 }
 
 showTime();
