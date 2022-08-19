b53d2a11e12c58733e69cf97ea9059b2e0cb36c3
Elias Cohen <eliascm1@gmail.com>
2022-08-19 17:38:51 -0500

    New PM vector

--- JS/about_me.js
+++ JS/about_me.js
@@ -1,4 +1,5 @@
 const curtain = gsap.timeline({defaults:{duration:.13, ease: "none"}})
+const cat = gsap.timeline({scrollTrigger: {trigger: '#paw', start: "bottom 80%"}, defaults:{duration:.5, ease: "none", transformOrigin: "50%% 50%"}})
 
 var i;
 var piece = "#piece";
@@ -11,4 +12,6 @@ curtain.to(piece.concat(j),{y:-44})
 curtain.to(piece.concat(j),{opacity:0, duration:0})
 }
 
-
+cat.to('#paw',{rotation:"45deg"},)
+cat.to('#paw',{rotation:"0"})
+cat.repeat(1);
