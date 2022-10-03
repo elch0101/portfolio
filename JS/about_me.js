f2c90063145311b17406c469a0db47df0ca62364
Elias Cohen <eliascm1@gmail.com>
2022-10-03 13:00:52 -0500

    Added variable years to about me page

--- JS/about_me.js
+++ JS/about_me.js
@@ -1,3 +1,4 @@
+
 const curtain = gsap.timeline({defaults:{duration:.13, ease: "none"}})
 const cat = gsap.timeline({scrollTrigger: {trigger: '#paw', start: "bottom 80%"}, defaults:{duration:.5, ease: "none", transformOrigin: "50%% 50%"}})
 
@@ -15,3 +16,18 @@ curtain.to(piece.concat(j),{opacity:0, duration:0})
 cat.to('#paw',{rotation:"45deg"},)
 cat.to('#paw',{rotation:"0"})
 cat.repeat(1);
+
+dt1 = new Date("July 31, 2020");
+dt2 = new Date();
+document.getElementById("years").innerHTML = diff_years(dt1, dt2)+"+";
+
+
+
+function diff_years(dt2, dt1) 
+ {
+
+  var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+   diff /= (60 * 60 * 24);
+  return Math.abs(Math.round(diff/365.25));
+   
+ }
